a862cdfeab6be71ab52711170c9c5e3c24bc0d44
HetPatel-0209 <12202080601055@adit.ac.in>
2024-03-21 23:37:10 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,207 @@
+// ignore_for_file: non_constant_identifier_names, avoid_print
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'CRUD Opes'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String Name='', StuID='', CCode='';
+  double Cgpa=0;
+  getStudentName(Name){
+    this.Name = Name;
+  }
+  getStudentID(StuID){
+    this.StuID = StuID;
+  }
+  getCourseCode(CCode){
+    this.CCode = CCode;
+  }
+  getCGPA(Cgpa){
+    this.Cgpa = double.parse(Cgpa);
+  }
+  CreateData(){
+    print('created');
+  }
+  ReadData(){
+    print('Read');
+  }
+  UpdateData(){
+    print('updated');
+  }
+  DeleteData(){
+    print('deleted');
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title,
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+        ),),
+      ),
+      body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: "Name",
+                  fillColor: Colors.white,
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)), 
+                  )
+                ),
+                onChanged: (String Name){
+                  getStudentName(Name);
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: "Student ID",
+                  fillColor: Colors.white,
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)), 
+                  )
+                ),
+                onChanged: (String StuID){
+                  getStudentID(StuID);
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: "Course Code",
+                  fillColor: Colors.white,
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)), 
+                  )
+                ),
+                onChanged: (String CCode){
+                  getCourseCode(CCode);
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: "CGPA",
+                  fillColor: Colors.white,
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)), 
+                  )
+                ),
+                onChanged: (String Cgpa){
+                  getCGPA(Cgpa);
+                },
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: ElevatedButton(onPressed:() {
+                    CreateData();
+                  },
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.greenAccent),
+                  ), 
+                  child: const Text('Create',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: ElevatedButton(onPressed:() {
+                    ReadData();
+                  },
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.blueAccent),
+                  ), 
+                  child: const Text('Read',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: ElevatedButton(onPressed:() {
+                    UpdateData();
+                  },
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.amberAccent),
+                  ), 
+                  child: const Text('Update',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: ElevatedButton(onPressed:() {
+                    DeleteData();
+                  },
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.redAccent),
+                  ), 
+                  child: const Text('Delete',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+ 
+    );
+  }
+}
